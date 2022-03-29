ad91e95fbc0009fdb4f384c3551b2dba0583cdbf
Wiley Lau <henloboii@protonmail.ch>
2022-03-29 18:12:36 +0800

    x505l: inherit 64bit configuration
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- lineage_X505L.mk
+++ lineage_X505L.mk
@@ -23,6 +23,7 @@ $(call inherit-product, device/lenovo/X505L/device.mk)
 
 # inherit LineageOS Configuration.
 #$(call inherit-product, build/target/product/product_launched_with_p.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, build/target/product/aosp_base.mk)
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
